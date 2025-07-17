2fb8b963ec9a0c73782aff3f1013a7f35391c7b5
jihanv <hedjmny@gmail.com>
2025-07-17 20:44:51 +0900

    Add other data stats information

--- src/Container.jsx
+++ src/Container.jsx
@@ -5,11 +5,19 @@ import Textarea from "./Textarea";
 export default function Container() {
   const [text, setText] = useState("");
   const numberOfCharacters = text.length;
+  const instagramCharactersLeft = 280 - text.length;
+  const facebookCharactersLeft = 2200 - text.length;
+  const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
 
   return (
     <main className="container">
       <Textarea text={text} setText={setText} />
-      <Stats numberOfCharacters={numberOfCharacters} />
+      <Stats
+        numberOfCharacters={numberOfCharacters}
+        instagramCharactersLeft={instagramCharactersLeft}
+        facebookCharactersLeft={facebookCharactersLeft}
+        numberOfWords={numberOfWords}
+      />
     </main>
   );
 }
