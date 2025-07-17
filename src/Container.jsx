fb97de7611bd10ed51207584f5073e023acfd39c
jihanv <hedjmny@gmail.com>
2025-07-17 21:14:31 +0900

    Extract constants into a separate file

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Stats from "./Stats";
 import Textarea from "./Textarea";
+import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from "./constants";
 
 export default function Container() {
   const [text, setText] = useState("");
   const numberOfCharacters = text.length;
-  const instagramCharactersLeft = 280 - text.length;
-  const facebookCharactersLeft = 2200 - text.length;
+  const instagramCharactersLeft = INSTAGRAM_MAX_CHARACTERS - text.length;
+  const facebookCharactersLeft = FACEBOOK_MAX_CHARACTERS - text.length;
   const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
 
   return (
